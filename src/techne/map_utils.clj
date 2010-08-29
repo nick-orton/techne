f3ad35f186c9ed627c1f2597f22430aa2143c2df
no <no@newt.(none)>
2010-08-29 18:08:15 -0400

    keep if with or without val

--- src/techne/map_utils.clj
+++ src/techne/map_utils.clj
@@ -2,12 +2,12 @@
 
 (defn keep-if
   ([key fun val map]
-   (if (fun val)
-       (assoc map key val)
-       (dissoc map key)))
+    (if (fun val)
+        (assoc map key val)
+        (dissoc map key)))
   ([key fun map]
-   (if (fun key)
-       (map)
-       (dissoc map key))))
+    (if (fun key)
+        map
+        (dissoc map key))))
 
 
--- test/techne/map_utils_test.clj
+++ test/techne/map_utils_test.clj
@@ -14,6 +14,7 @@
 (deftest test-keep-if
   (is (= {:a 2} (keep-if :a always-true 2 {})))
   (is (= {} (keep-if :a always-false 2 {:a 2})))
+  (is (= {:a 2} (keep-if :a always-true {:a 2})))
   (is (= {} (keep-if :a always-false {:a 2})))
   
          )
