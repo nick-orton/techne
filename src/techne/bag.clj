114b11e47ca7cfd4017a6c94b09c0d939545fd14
no <no@newt.(none)>
2010-09-18 22:57:44 -0400

    REName get-n to count

--- src/techne/bag.clj
+++ src/techne/bag.clj
@@ -1,29 +1,35 @@
 (ns techne.bag
-  (:use [techne.map-utils]))
+  (:use [techne.map-utils])
+  (:import java.util.Map))
 
 (defprotocol Bag
   (put-n [self item n])
   (put [self item])
   (pluck-n [self item n])
   (pluck [self item])
-  (get-n [self item]))
+  ;(count [self])
+  (count [self item]))
 
 (defrecord MapBag [state]
   Bag
     (put-n [self item n] 
-      (let [new-n (+ n (get-n self item))]
+      (let [new-n (+ n (count self item))]
         (MapBag. (assoc state item new-n))))
     (put [self item]
       (put-n self item 1))
-    (get-n [self item] 
+
+    (count [self item] 
       (get state item 0))
     (pluck-n [self item n]
-      (let [prev (get-n self item)
+      (let [prev (count self item)
             new-n (- prev n)]
            (MapBag. (keep-if item pos? new-n state))))
     (pluck [self item]
       (pluck-n self item 1))
 
+;  java.util.Map 
+;    (getKeys [self] (keys state)) ;TODO TEST
+           
   Object
     (toString [self]
       (str ("Bag: " (:state self)))))
