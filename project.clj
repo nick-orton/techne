00e2bbb4fdfe20c24c956257e9105f252c571da7
Nick Orton <orton.nick@gmail.com>
2011-05-11 10:40:35 -0400

    starting 7.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techne "0.6.1"
+(defproject techne "0.7.0-SNAPSHOT"
   :description "a collection of utilities and macros"
   :dev-dependencies [[lein-clojars "0.6.0"]]
   :dependencies [[org.clojure/clojure "1.2.0"]
