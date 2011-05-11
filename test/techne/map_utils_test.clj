25258cbfed5bc1b8503a5b8ef88f57c30a54a016
Nick Orton <orton.nick@gmail.com>
2011-05-11 10:49:13 -0400

    fix test name

--- test/techne/map_utils_test.clj
+++ test/techne/map_utils_test.clj
@@ -14,7 +14,7 @@
   (is (= {:a 2} (keep-key-if {:a 2} always-true :a )))
   (is (= {} (keep-key-if {:a 2} always-false :a))))
 
-(deftest test-keep-if
+(deftest test-remove-key-with
   (is (= {:a 2} (remove-key-if {:a 2} always-false :a)))
   (is (= {} (remove-key-if {:a 2} always-true :a ))))
 
