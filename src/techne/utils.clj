6d55578a31e8d5a0a234a33d18939c6b69254d07
Nick Orton <orton.nick@gmail.com>
2011-05-10 21:49:15 -0400

    adding doc strings

--- src/techne/utils.clj
+++ src/techne/utils.clj
@@ -4,6 +4,8 @@
 
 
 (defmacro def-bang-form
+  "takes a predicate as an argument and defines its inverse identified by a 
+   preceding '!'"
   [fun]
   `(defn  ~(symbol (str "!" fun)) 
      [& as#] 
@@ -13,18 +15,22 @@
 (def-bang-form nil?)
 
 (defn insert-2nd 
+  "insert a value behind the head of a sequence"
   [x xs]
   (cons (first xs) (cons x (rest xs))))
 
 (defn +strs
+  "concatinate a sequence of strings together"
   [strings]
   (str/join "" strings))
 
 (defn swap-if
+  {:deprecated "0.6.1"}
   [pred token element]
   (if (pred element) token element))
 
 (defn swap-if-eq 
+  {:deprecated "0.6.1"}
   [target token element]
   (swap-if #(= target %) token element))
 
