aa4bcb3e19a34939428fced0df555e07de66dd50
Nick Orton <orton.nick@gmail.com>
2011-05-12 11:40:43 -0400

    removing deprecated methods from utils

--- src/techne/utils.clj
+++ src/techne/utils.clj
@@ -3,42 +3,24 @@
   )
 
 (defmacro def-bang-form
-  "takes a predicate as an argument and defines its inverse identified by a 
+  "takes a predicate as an argument and defines its inverse identified by a
    preceding '!'
-  
-   
+
        user=> (def-bang-form =)
        #'user/!=
        user=> (!= 0 1)
        true
-       user=> 
+       user=>
   "
   [fun]
-  `(defn  ~(symbol (str "!" fun)) 
-     [& as#] 
+  `(defn ~(symbol (str "!" fun))
+     [& as#]
      (not (apply ~fun as#))))
 
 (def-bang-form =)
 (def-bang-form nil?)
 
-(defn insert-2nd 
+(defn insert-2nd
   "insert a value behind the head of a sequence"
   [x xs]
   (cons (first xs) (cons x (rest xs))))
-
-(defn +strs
-  "concatinate a sequence of strings together"
-  {:deprecated "0.6.1"}
-  [strings]
-  (apply str strings))
-
-(defn swap-if
-  {:deprecated "0.6.1"}
-  [pred token element]
-  (if (pred element) token element))
-
-(defn swap-if-eq 
-  {:deprecated "0.6.1"}
-  [target token element]
-  (swap-if #(= target %) token element))
-
