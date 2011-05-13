796dcacfe96c79d1f66e47d4143429483732cdc0
Nick Orton <orton.nick@gmail.com>
2011-05-13 02:33:46 -0400

    adding url

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject techne "0.7.0-SNAPSHOT"
   :description "a collection of utilities and macros"
+  :url "https://github.com/nick-orton/techne"
   :dev-dependencies [[lein-clojars "0.6.0"]
                      [marginalia "0.5.1"]]
   :dependencies [[org.clojure/clojure "1.2.0"]
