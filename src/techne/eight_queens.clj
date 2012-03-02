6078f4bc24cd064420677f070456acb531abd667
Nick Orton <orton.nick@gmail.com>
2012-03-01 21:42:07 -0500

    commenting

--- src/techne/eight_queens.clj
+++ src/techne/eight_queens.clj
@@ -2,10 +2,14 @@
 ; that no queen is positioned to capture another.  We will represent the board
 ; as a vector where each index represents a column and its value represents
 ; the row that the queen is placed on.
-
+;
 (ns techne.eight-queens)
 
-
+; A queen can be in violation in 1 of 3 ways
+; 1. on the same column as another queen (impossible in our solution)
+; 2. on the same row as another queen
+; 3. on a diagonal from a queen.
+;
 (defn- violation? [board row]
   (loop [cols (reverse board)
          i 1]
@@ -19,6 +23,11 @@
       true
         (recur (rest cols) (inc i)))))
 
+; Try to place a queen on a board.  If the queen is off the board, then back
+; up and try the column previous one row up.  If the queen violates the rules,
+; try to place it one row up.  If the queen is not in violation and the board
+; is full return.  Otherwise try to place a queen on the next column.
+;
 (defn place [board row]
   (cond
     (= row 8)
