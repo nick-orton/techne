60b39169c9fda8c7e814044e8a9149842e8907b8
nick orton <nick@orton.biz>
2012-05-31 09:10:47 -0400

    adding line intersections

---
+++ src/techne/geometria/transformations.clj
@@ -0,0 +1,9 @@
+(ns techne.geometria.transformations
+  (:use [techne.geometria.elements :only [offset slope point]]))
+
+
+(defn intersection [line1 line2]
+  (let [x (/ (- (offset line2) (offset line1))
+             (- (slope line1) (slope line2)))
+        y (+ (* (slope line1) x) (offset line1))]
+       (point x y) ))
---
+++ test/techne/geometria/transformations_test.clj
@@ -0,0 +1,14 @@
+(ns techne.geometria.transformations-test
+  (:use [clojure.test]
+        [techne.geometria.elements :only [line x-cord y-cord]]
+        [techne.geometria.transformations]))
+
+(deftest intersection-test
+  (let [line1 (line  3 1)
+        line2 (line -1 6)
+        p     (intersection line1 line2)]
+    (is (= 5/4 (x-cord p)))
+    (is (= 19/4 (y-cord p)))))
+
+
+
