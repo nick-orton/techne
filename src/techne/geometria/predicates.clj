06b800d2f75e46a066fcdf21a38bbc7880844934
nick orton <nick@orton.biz>
2012-05-31 09:54:55 -0400

    point on a segment :: needs refactoring

--- src/techne/geometria/predicates.clj
+++ src/techne/geometria/predicates.clj
@@ -1,6 +1,34 @@
 (ns techne.geometria.predicates
-  (:use [techne.geometria.elements :only [offset slope x-cord y-cord]]))
+  (:use [techne.geometria.elements :only [offset slope x-cord y-cord segment->line points]]))
 
 
 (defn point-on-line? [point line]
-  (== (y-cord point) (+ (* (slope line) (x-cord point)) (offset line))))
+  (= (y-cord point) (+ (* (slope line) (x-cord point)) (offset line))))
+
+
+(defn high-point [segment]
+  (let [y (apply max (map y-cord (points segment)))]
+    (first (filter #(= (y-cord %) y) (points segment)))))
+
+(defn low-point [segment] nil
+   (let [y (apply min (map y-cord (points segment)))]
+    (first (filter #(= (y-cord %) y) (points segment)))))
+
+
+(defn left-point [segment] nil
+  (let [x (apply min (map x-cord (points segment)))]
+    (first (filter #(= (x-cord %) x) (points segment)))))
+
+
+(defn right-point [segment] nil
+  (let [x (apply max (map x-cord (points segment)))]
+    (first (filter #(= (x-cord %) x) (points segment)))))
+
+
+(defn point-on-line-segment? [point segment]
+  (let [line (segment->line segment)]
+    (and (point-on-line? point line)
+         (>= (y-cord (high-point segment)) (y-cord point))
+         (<= (y-cord (low-point segment)) (y-cord point))
+         (<= (x-cord (left-point segment)) (x-cord point))
+         (>= (x-cord (right-point segment)) (x-cord point)))))
--- test/techne/geometria/predicates_test.clj
+++ test/techne/geometria/predicates_test.clj
@@ -1,6 +1,6 @@
 (ns techne.geometria.predicates-test
   (:use [clojure.test]
-        [techne.geometria.elements :only [line point]]
+        [techne.geometria.elements :only [line point line-segment]]
         [techne.geometria.predicates]))
 
 (deftest point-on-line-test
@@ -10,6 +10,17 @@
     (is (point-on-line? point-on line))
     (is (not (point-on-line? point-off line)))))
 
-;TODO point-on-line-segment? point line-segment
+(deftest point-on-line-segment
+  (let [seg                      (line-segment (point 0 0) (point 5 5))
+        point-on                 (point 1 1)
+        point-off-line           (point 3 4)
+        point-too-far            (point 6 6)
+        point-too-far-other-side (point -1 -1)]
+
+    (is (point-on-line-segment? point-on seg))
+    (is (not (point-on-line-segment? point-off-line seg)))
+    (is (not (point-on-line-segment? point-too-far seg)))
+    (is (not (point-on-line-segment? point-too-far-other-side seg)))))
+
 ;TODO point-in-circle? point circle
 ;TODO intersect? line-segment line-segment
